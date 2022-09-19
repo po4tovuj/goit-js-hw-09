510344cd6e48900cf48deac4215a63933b25c496
Sergey Miniaylo <po4tovuj@gmail.com>
2022-09-19 11:11:44 +0300

    done task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+let timerId = null;
+const changeBodyBgColor = () => {
+  const color = getRandomHexColor();
+  body.style.backgroundColor = color;
+};
+startBtn.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    changeBodyBgColor();
+    startBtn.setAttribute('disabled', '');
+  }, 1000);
+});
 
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+  startBtn.removeAttribute('disabled');
+});
