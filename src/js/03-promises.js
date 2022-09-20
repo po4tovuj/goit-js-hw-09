9c7d85830414dea2bf5f5d2bcb7d7e8bf96590b8
Sergey Miniaylo <po4tovuj@gmail.com>
2022-09-20 10:33:39 +0300

    done task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,46 @@
+import { Notify } from 'notiflix';
+
+const form = document.querySelector('form');
+form.addEventListener('submit', onSubmit);
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    // Fulfill
+    return Promise.resolve().then(() => {
+      setTimeout(() => {
+        Notify.success(`Fulfilled promise ${position} in ${delay}ms`, {
+          position: 'center-top',
+          fontSize: '20px',
+          width: '370px',
+        });
+      }, delay);
+    });
   } else {
-    // Reject
+    return Promise.reject().catch(() => {
+      setTimeout(() => {
+        Notify.failure(`Rejected promise ${position} in ${delay}ms`, {
+          position: 'center-top',
+          fontSize: '20px',
+          width: '370px',
+        });
+      }, delay);
+    });
   }
+}
+
+function onSubmit(e) {
+  e.preventDefault();
+  const {
+    elements: {
+      delay: { value: delay },
+      step: { value: step },
+      amount: { value: amount },
+    },
+  } = e.currentTarget;
+
+  let nextDelay = +delay;
+  const promises = Array.from(Array(+amount)).map((item, i) => {
+    nextDelay += i ? Number(step) : 0;
+    createPromise(i, nextDelay);
+  });
+  Promise.allSettled(promises);
 }
