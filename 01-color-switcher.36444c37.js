b22fe075b7fcb23c5bf202b2c072c0f1e4f6e515
po4tovuj <po4tovuj@gmail.com>
2022-09-22 17:15:26 +0000

    Deploying to gh-pages from @ po4tovuj/goit-js-hw-09@eaa16ad79e8c1b14c4aba0047f0e6b14ecb42111 🚀

---
+++ 01-color-switcher.36444c37.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=document.querySelector("body"),o=null,r=function(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16));n.style.backgroundColor=t};t.addEventListener("click",(function(){t.setAttribute("disabled",""),o=setInterval((function(){r()}),1e3)})),e.addEventListener("click",(function(){clearInterval(o),t.removeAttribute("disabled")}))}();
+//# sourceMappingURL=01-color-switcher.36444c37.js.map
